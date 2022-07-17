aeb8f88441df647345e54cdc731f8a1e146755fd
Mariyan Damyanov <60383290+marians1d@users.noreply.github.com>
2022-07-17 23:33:37 +0300

    Removing moving logic to controllers

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,21 +1,32 @@
 const orders = require('../services/orders');
+const Order = require('../models/order');
 
 async function getAll(req, res) {
-    res.json(await orders.getAll())
+    res.json(await Order.find({}))
 }
 
 async function getById(req, res) {
     const id = req.params.id;
 
-    const result = await orders.getById(id);
+    const order = Order.findById(id);
 
-    return res.json(result);
+    return res.json(order);
 }
 
 async function add(req, res) {
     const { title, description, price, image, type, status } = req.body;
 
-    const result = await orders.add({ title, description, price, image, type, status })
+    const result = new Order({
+        title,
+        description,
+        price,
+        image,
+        type,
+        status
+    });
+
+
+    await result.save();
 
     res.json(result);
 }
@@ -26,9 +37,27 @@ async function updateById(req, res) {
     const { title, description, price, image, type, status } = req.body;
 
     try {
-        const result = await orders.updateById(id, { title, description, price, image, type, status });
+        const existing = await Order.findById(id);
 
-        return res.json(result);
+        if (existing) {
+            existing.title = title;
+            existing.description = description;
+            existing.price = price;
+            existing.image = image;
+            existing.type = type;
+            existing.status = status;
+
+            await existing.save();
+
+            return existing
+        } else {
+            const error = new Error('Not Found');
+            error._notFound;
+
+            throw error;
+        }
+
+        return res.json(existing);
     } catch (error) {
         if (error._notFound) {
             res.status(404).json({ message: 'Order not found' });

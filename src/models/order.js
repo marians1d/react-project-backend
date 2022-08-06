0c56f80ee36ed3f320ece2b9066bffd5e7b80c00
Mariyan Damyanov <60383290+marians1d@users.noreply.github.com>
2022-08-06 19:29:30 +0300

    Updating order schema

--- src/models/order.js
+++ src/models/order.js
@@ -3,10 +3,28 @@ const { Schema, model, Types: { ObjectId } } = require('mongoose');
 const orderSchema = new Schema({
     title: { type: String },
     description: { type: String },
+    address: {
+        type: String,
+    },
+    visibility: {
+        type: String,
+        enum: [
+            'private',
+            'public'
+        ],
+        required: true
+    },
+    appointedEmployes: [{
+        type: ObjectId,
+        ref: 'User'
+    }],
     price: { type: Number },
-    image: { type: String },
+    imageUrl: { type: String },
     type: { type: String },
     status: { type: String },
+    measurementDate: {
+        type: Date
+    },
     ownerId: { type: ObjectId, ref: 'User' }
 });
 

a867e5586f556cc13dd0340d5092491df78e6894
Mariyan Damyanov <marians1md@gmail.com>
2022-08-18 23:43:21 +0300

    Added error status

--- src/middlewares/guards.js
+++ src/middlewares/guards.js
@@ -3,17 +3,17 @@ module.exports = {
         if (req.user) {
             next();
         } else {
-            res.status(401).json({ message: 'Please log in' });
+            res.status(401).json({ status: 'error', message: 'Please log in' });
         }
     },
 
     isOwner: () => (req, res, next) => {
         if (!req.user) {
-            res.status(401).json({ message: 'Please log in' });
+            res.status(401).json({ status: 'error', message: 'Please log in' });
         } else if (req.user._id == res.locals.order.ownerId) {
             next();
         } else {
-            res.status(403).json({ message: 'You cannot modify this record' });
+            res.status(403).json({ status: 'error', message: 'You cannot modify this record' });
         }
     }
 };

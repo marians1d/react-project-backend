f6cad1e1a25dad7e2cd3f9c4c9f507044da238c0
Mariyan Damyanov <60383290+marians1d@users.noreply.github.com>
2022-08-19 10:51:25 +0300

    Fix secret

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_secret || 'fa;sjdfioeuirjfioewureio;ruewirj';
+const JWT_SECRET = process.env.JWT_SECRET || 'fa;sjdfioeuirjfioewureio;ruewirj';
 
 function createToken(data) {
     return jwt.sign(data, JWT_SECRET, { expiresIn: '1d' });

50694a9047565785daef8a4f462e1e3d42268211
Mariyan Damyanov <60383290+marians1d@users.noreply.github.com>
2022-08-19 13:57:15 +0300

    Update default port

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 3001,
         dbURL: process.env.DB_URL_CREDENTIALS || 'mongodb://localhost:27017/custom-furniture',
         origin: ['http://127.0.0.1:5500', 'http://localhost:3030', 'http://localhost:3000']
     },
     production: {
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 3001,
         dbURL: process.env.DB_URL_CREDENTIALS,
         origin: ['http://localhost:3000', 'https://dream-style.web.app']
     }

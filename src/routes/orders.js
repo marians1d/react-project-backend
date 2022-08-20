f560d771856fad7063d3c8daa4dd6bbf24e22687
Mariyan Damyanov <marians1md@gmail.com>
2022-08-20 17:31:49 +0300

    Adding order image uploads

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const formidable = require('express-formidable');
 
 const { isAuth, isOwner } = require('../middlewares/guards');
 const preload = require('../middlewares/preload');
@@ -11,6 +12,7 @@ const orders = require('../controllers/orders');
 router.get('/', requestHandler(orders.getAll));
 router.get('/personal', isAuth(), requestHandler(orders.getPersonal));
 router.post('/', isAuth(), requestHandler(orders.add));
+router.post('/image', isAuth(), formidable(), requestHandler(orders.addImage));
 router.get('/:id', requestHandler(orders.getById));
 router.put('/:id', preload(api), isOwner(), requestHandler(orders.updateById));
 router.delete('/:id', isAuth(), preload(api), isOwner(), requestHandler(orders.del));
